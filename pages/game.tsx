601a6ade6b33137ac3e0a75f9f87930c1cf06048
Aphiwad Chhoeun <aphiwad.chhoeun@gmail.com>
2022-06-25 22:00:39 +0000

    implement win streak & checkout

--- pages/game.tsx
+++ pages/game.tsx
@@ -8,6 +8,7 @@ const Game: NextPage = ({ number }: any) => {
     const [oldSecret, setOldSecret] = useState<number>(secret)
     const [oldOldSecret, setOldOldSecret] = useState<number>(secret)
     const [streak, setStreak] = useState<number>(0)
+    const [scoreBuffer, setScoreBuffer] = useState<number>(0)
 
     function generateNextNumber(): number {
         return randomNumber(0, 100)
@@ -23,9 +24,13 @@ const Game: NextPage = ({ number }: any) => {
 
     function handleResult(test: Function) {
         if (test()) {
-            setScore(score + 1)
-            setStreak(streak + 1)
+            const tempStreak = streak + 1
+            let tempScoreBuffer = scoreBuffer + 1
+            tempScoreBuffer += scoreBuffer * streak
+            setScoreBuffer(tempScoreBuffer)
+            setStreak(tempStreak)
         } else {
+            setScoreBuffer(0)
             setStreak(0)
         }
     }
@@ -49,17 +54,25 @@ const Game: NextPage = ({ number }: any) => {
         populateSecret(newSecret)
     }
 
+    function handleCheckoutClick() {
+        setScore(score + scoreBuffer)
+        setScoreBuffer(0)
+        setStreak(0)
+    }
+
     return (
         <div className={styles.container}>
             <main className={styles.main}>
                 <div className={styles.title}>Hi-low Game</div>
                 <div className={styles.gameWrapper}>
                     <div className={styles.score}>Score: {score}</div>
+                    <div className={styles.score}>Win: {scoreBuffer}</div>
                     <div><span className={styles.oldSecret}>{oldOldSecret} -&gt; {oldSecret} -&gt;</span> <span className={styles.newSecret}>{secret}</span></div>
                     <div className={styles.gameControls}>
                         <button className={styles.button} onClick={() => handleSkipClick()}>Skip</button>
                         <button className={styles.button} onClick={() => handleHighClick()}>High</button>
                         <button className={styles.button} onClick={() => handleLowClick()}>Low</button>
+                        <button className={styles.button} onClick={() => handleCheckoutClick()}>Checkout</button>
                     </div>
                     {streak > 0 && <div>Winning Streak x{streak}</div>}
                 </div>
